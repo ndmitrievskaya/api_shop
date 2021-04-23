4e6e33148eaa0769c48085465b19971db16eea3b
Veronika Dmitrievskaya <nika.dmitrievskaya@gmail.com>
2021-04-23 16:42:37 +0300

    Implement category validation

--- goods/models.py
+++ goods/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(unique=True, max_length=300)
 
     def __str__(self):
         return self.name
@@ -10,6 +11,7 @@ class Category(models.Model):
 
 class Good(models.Model):
     name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, max_length=300)
     category = models.ManyToManyField('Category', related_name='goods')
     price = models.FloatField()
     is_published = models.BooleanField(default=False)
--- goods/serializers.py
+++ goods/serializers.py
@@ -1,16 +1,16 @@
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
-from rest_framework.fields import SerializerMethodField
 
 from .models import Good, Category
 
 
 class GoodListSerializer(serializers.ModelSerializer):
-    # category = SerializerMethodField('get_limited_number_of_categories')
-    #
-    # def get_limited_number_of_categories(self, obj):
-    #     qs = obj.category.all()
-    #     if len(qs) > 10:
-    #         return ValueError
+
+    def validate_category(self, category):
+        if len(category) < 2 or len(category) > 10:
+            raise serializers.ValidationError(
+                "You should choose from 2 to 10 categories to add a good")
+        return category
 
     class Meta:
         model = Good
