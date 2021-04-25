ecc73cc812515ddee6dac4640330de2a314273f3
Veronika Dmitrievskaya <nika.dmitrievskaya@gmail.com>
2021-04-25 23:34:38 +0300

    Implement goods and categories tests

--- goods/tests.py
+++ goods/tests.py
@@ -1,13 +1,9 @@
-import json
-import os
-
-import django
 from rest_framework import status
 from django.test import TestCase, Client
 from django.urls import reverse
 
-from .models import Good
-from .serializers import GoodListSerializer
+from .models import Good, Category
+from .serializers import GoodListSerializer, CategoryListSerializer
 
 client = Client()
 
@@ -29,5 +25,45 @@ class GetAllGoodsTest(TestCase):
         response = client.get(reverse('good-list'))
         goods = Good.objects.all()
         serializer = GoodListSerializer(goods, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_good_by_slug(self):
+        response = client.get(
+            reverse('good_slug', kwargs={'slug': 'phone'}))
+        good = Good.objects.get(slug='bath')
+        serializer = CategoryListSerializer(good)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class GetAllCategoriesTest(TestCase):
+    def setUp(self):
+        Category.objects.create(
+            name='Bath', slug='bath')
+        Category.objects.create(
+            name='Clothes', slug='clothes')
+        Category.objects.create(
+            name='Electronics', slug='electronics')
+
+    def test_get_all_categories(self):
+        response = client.get(reverse('category-list'))
+        categories = Category.objects.all()
+        serializer = CategoryListSerializer(categories, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_first_categories(self):
+        response = client.get(reverse('category-detail', kwargs={'pk': 1}))
+        category = Category.objects.get(pk=1)
+        serializer = CategoryListSerializer(category)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_category_by_slug(self):
+        response = client.get(
+            reverse('category_slug', kwargs={'slug': 'bath'}))
+        category = Category.objects.get(slug='bath')
+        serializer = CategoryListSerializer(category)
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
